351a09e4a182c1d681b4b69105f2f335379a82a3
AndresL99 <andreslerner99@gmail.com>
2025-11-25 01:52:39 -0300

    feat: add corsConfig and modified yml

--- src/main/java/com/starwars/api/configuration/ApiConfiguration.java
+++ src/main/java/com/starwars/api/configuration/ApiConfiguration.java
@@ -2,6 +2,8 @@ package com.starwars.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.net.http.HttpClient;
 
@@ -13,5 +15,17 @@ public class ApiConfiguration {
         return HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig()
+        {
+        return new WebMvcConfigurer(){
+            public void addCorsMapping(CorsRegistry corsRegistry)
+            {
+                corsRegistry.addMapping("/**")
+                        .allowedMethods("GET", "POST");
+            }
+        };
     }
 }
